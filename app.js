bf53627cc2f520dfe5debe8104c18beb2b868be1
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-28 07:24:52 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+var express = require( 'express' );
+var app = express();
+var serveStatic = require( 'serve-static' );
+var server = require( 'http' ).Server( app );
+var io = require( 'socket.io' )( server );
+
+app.use( serveStatic( __dirname + '/public' ) );
+
+app.get( '/', function( req, res ) {
+    res.sendFile( __dirname + '/public/index.html' );
+});
+
+server.listen( 3000 );
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+;(function( io ) {
+
+})( io );
