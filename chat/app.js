a0832c211a55b63b9c472846f8aeca53bde92649
Fernando Daciuk <f.daciuk@gmail.com>
2015-06-17 07:29:29 -0300

    Create structure to sent message from client to server

--- chat/app.js
+++ chat/app.js
@@ -11,5 +11,16 @@ app.get( '/', function( req, res ) {
   res.sendFile( __dirname + '/public/index.html' );
 });
 
+io.on( 'connection', function( socket ) {
+  console.log( 'a user connected' );
+  socket.on( 'disconnect', function() {
+    console.log( 'user disconnected' );
+  });
+
+  socket.on( 'chat message', function( data ) {
+    console.log( data );
+  });
+});
+
 server.listen( 3000 );
 console.log( 'The chat magic is happen on http://localhost:3000' );
--- chat/public/js/main.js
+++ chat/public/js/main.js
@@ -1,3 +1,29 @@
-;(function( io ) {
+;(function( io, undefined ) {
+  function app() {
+    var $public = {};
+    var $private = {};
 
+    $private.socket = io();
+    $private.$form = document.querySelector( 'form' );
+
+    $public.init = function init() {
+      $private.initEvents();
+    };
+
+    $private.initEvents = function initEvents() {
+      $private.$form.addEventListener( 'submit', $private.handleSubmit, false );
+    };
+
+    $private.handleSubmit = function handleSubmit( e ) {
+      debugger;
+      e.preventDefault();
+      var $message = document.querySelector( '#m' );
+      $private.socket.emit( 'chat message', $message.value );
+      $message.value = '';
+    };
+
+    return $public;
+  }
+
+  return app().init();
 })( io );
