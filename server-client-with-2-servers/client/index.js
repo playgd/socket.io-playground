00bdff72808fef95671cd3b724c93f2c30a40331
Fernando Daciuk <f.daciuk@gmail.com>
2015-11-19 15:23:14 -0200

    Create client

---
+++ server-client-with-2-servers/client/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let port = 5002;
+let io = require('socket.io')(port);
+
+io.on('connection', (socket) => {
+  console.log('io client connected!');
+
+  socket.emit('myClientEvent', 'Message sent by client.');
+  socket.on('myServerEvent', (data) => {
+    console.log('MESSAGE RECEIVED FROM SERVER:', data);
+  });
+});
