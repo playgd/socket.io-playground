0cb048dc916cf61aa5da76fb59f9e106edc27d0c
Fernando Daciuk <f.daciuk@gmail.com>
2015-11-19 15:37:46 -0200

    Add disconnect event

--- server-client-with-2-servers/client/index.js
+++ server-client-with-2-servers/client/index.js
@@ -3,11 +3,16 @@
 let port = 5002;
 let io = require('socket.io')(port);
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
   console.log(`io client connected on port ${port}!`);
 
   socket.emit('myClientEvent', 'Message sent by client.');
   socket.on('myServerEvent', (data) => {
     console.log('MESSAGE RECEIVED FROM SERVER:', data);
   });
+});
+
+
+io.on('disconnect', () => {
+  console.log('io client disconnected!');
 });
--- server-client-with-2-servers/server/index.js
+++ server-client-with-2-servers/server/index.js
@@ -1,16 +1,18 @@
 'use strict';
 
-let port = 5001;
 let clientPort = 5002;
-let io = require('socket.io')(port);
 let ioClient = require('socket.io-client');
 var socketClient = ioClient.connect(`http://localhost:${clientPort}`);
 
 socketClient.on('connect', () => {
-  console.log(`io server connected on port ${port}!`);
+  console.log(`io server connected from client on port ${clientPort}!`);
   socketClient.emit('myServerEvent', 'Message sent by server.');
 });
 
 socketClient.on('myClientEvent', (data) => {
   console.log('MESSAGE RECEIVED FROM CLIENT:', data);
 });
+
+socketClient.on('disconnect', () => {
+  console.log('io server disconnected from client!');
+})
