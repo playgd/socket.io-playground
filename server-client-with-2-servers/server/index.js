ecbbd971de041bbafb5fb434db033dce226a9f32
Fernando Daciuk <f.daciuk@gmail.com>
2015-11-19 15:41:44 -0200

    Improve server code

--- server-client-with-2-servers/server/index.js
+++ server-client-with-2-servers/server/index.js
@@ -1,18 +1,17 @@
 'use strict';
 
 let clientPort = 5002;
-let ioClient = require('socket.io-client');
-var socketClient = ioClient.connect(`http://localhost:${clientPort}`);
+let ioClient = require('socket.io-client')(`http://localhost:${clientPort}`);
 
-socketClient.on('connect', () => {
+ioClient.on('connect', () => {
   console.log(`io server connected from client on port ${clientPort}!`);
-  socketClient.emit('myServerEvent', 'Message sent by server.');
+  ioClient.emit('myServerEvent', 'Message sent by server.');
 });
 
-socketClient.on('myClientEvent', (data) => {
+ioClient.on('myClientEvent', (data) => {
   console.log('MESSAGE RECEIVED FROM CLIENT:', data);
 });
 
-socketClient.on('disconnect', () => {
+ioClient.on('disconnect', () => {
   console.log('io server disconnected from client!');
 })
