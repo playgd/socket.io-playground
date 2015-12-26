8b8fde45033dfcf3295d00492444456378776ddf
Fernando Daciuk <f.daciuk@gmail.com>
2015-12-26 10:31:54 -0200

    Fix code styling

--- chat/socket.io/io.js
+++ chat/socket.io/io.js
@@ -1,28 +1,28 @@
 'use strict';
 
-module.exports = function( io ) {
+module.exports = function(io) {
   function init() {
     initSocketEvents();
   }
 
   function initSocketEvents() {
-    io.on( 'connection', function( socket ) {
-      console.log( 'a user connected' );
-      socket.on( 'disconnect', handleDisconnect );
-      socket.on( 'chat message', handleChatMessage );
-      socket.on( 'is typing', handleTyping );
+    io.on('connection', function(socket) {
+      console.log('a user connected');
+      socket.on('disconnect', handleDisconnect);
+      socket.on('chat message', handleChatMessage);
+      socket.on('is typing', handleTyping);
     });
   }
 
   function handleDisconnect() {
-    console.log( 'user disconnected' );
+    console.log('user disconnected');
   }
 
-  function handleChatMessage( msg ) {
-    this.broadcast.emit( 'chat message', msg );
+  function handleChatMessage(msg) {
+    this.broadcast.emit('chat message', msg);
   }
 
-  function handleTyping( typing ) {
+  function handleTyping(typing) {
     this.broadcast.emit( 'is typing', typing );
   }
 
