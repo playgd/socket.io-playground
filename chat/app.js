1252f8498520c28e7f8159a72b09f20d9499aa88
Fernando Daciuk <f.daciuk@gmail.com>
2015-12-27 10:33:13 -0200

    Fix code styling

--- chat/app.js
+++ chat/app.js
@@ -1,17 +1,17 @@
 'use strict';
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var serveStatic = require( 'serve-static' );
-var server = require( 'http' ).Server( app );
-var io = require( 'socket.io' )( server );
+var serveStatic = require('serve-static');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.use( serveStatic( __dirname + '/public' ) );
+app.use(serveStatic(__dirname + '/public'));
 
-app.get( '/', function( req, res ) {
-  res.sendFile( __dirname + '/public/index.html' );
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-require( './socket.io/io' )( io );
+require('./socket.io/io')(io);
 
-server.listen( 3000 );
-console.log( 'The chat magic is happen on http://localhost:3000' );
+server.listen(3000);
+console.log('The chat magic is happen on http://localhost:3000');
